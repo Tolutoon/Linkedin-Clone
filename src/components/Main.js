7e1bc7e5df6cf07ad5b1300d7d6dfff3fe5aaf78
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-05-15 22:51:05 +0100

    Home Page - Last Card Component Added

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,4 +8,10 @@ const Container = styled.div`
     grid-area:  main;
 `;
 
+const CommonCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+`;
+
 export default Main;
